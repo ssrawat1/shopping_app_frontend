dd39b4cc9733c0e0ed571c22d68538ab414ea266
ssrawat1 <ssr911999@gmail.com>
2026-03-02 13:43:43 +0530

    remove temporary logs

--- app/page.js
+++ app/page.js
@@ -4,11 +4,6 @@ import React from 'react'
 export const dynamic = "force-dynamic"
 
 const page = () => {
-  // const randomNumber = Math.random();
-  // console.log({ randomNumber })
-  // if (randomNumber > 0.5) {
-  //   throw new Error("something went wrong")
-  // }
   return (
     <Products />
   )
